089df77ff68d5cc87f2f55dd75c63068a887127f
Marty Schoch <marty.schoch@gmail.com>
2020-08-13 11:23:28 -0400

    [BP] remove TooManyClauses limitation when optimizable (#1426)
    Observation: When a search does not perform scoring and does not
    need location information, disjunction queries are optimized by
    directly OR'ing the underlying bitset.  This avoids all the
    usual multi-iterator disjunction logic.  However, in it's
    traditional form, we still have a TooManyClauses limit, and
    this makes sense as all the underlying iterators are still
    in memory at one time.
    
    Observation: The MultiTerm search is unique in that we have a
    flat list of terms that are used to build the disjunction.
    This is significant because it means we can ensure that
    all the underlying searchers are optimizable.
    
    By combining these two observations we can introduce a new mode
    of operation for the MultiTerm search.  When it does not perform
    scoring and does not need location information, we can do a new
    optimization where we create smaller batches of disjunctions
    which are immediately optimizable into a single term searcher.
    By repeating this process across all terms, we end up with
    the correct searcher, and we never had more than the batch
    size iterators built in memory at one time.
    
    UnadornedPostingsIteratorBitmap was refactored to also
    implement OptimizablePostingsIterator, this allows us
    to keep the in-progress final iterator in each batch,
    simplifying the logic.
    
    A new optimization mode "disjunction:unadorned-force" was
    introduced.  It behaves exacdtly the same as
    "disjunction:unadorned" only it always performs the
    optimization without regard for the cardinality of the
    underlying iterators.

--- index/scorch/optimize.go
+++ index/scorch/optimize.go
@@ -16,12 +16,12 @@ package scorch
 
 import (
 	"fmt"
-
 	"github.com/RoaringBitmap/roaring"
 
 	"github.com/blevesearch/bleve/index"
 	"github.com/blevesearch/bleve/index/scorch/segment"
 	"github.com/blevesearch/bleve/index/scorch/segment/zap"
+	"sync/atomic"
 )
 
 var OptimizeConjunction = true
@@ -39,7 +39,11 @@ func (s *IndexSnapshotTermFieldReader) Optimize(kind string,
 	}
 
 	if OptimizeDisjunctionUnadorned && kind == "disjunction:unadorned" {
-		return s.optimizeDisjunctionUnadorned(octx)
+		return s.optimizeDisjunctionUnadorned(octx, OptimizeDisjunctionUnadornedMinChildCardinality)
+	}
+
+	if OptimizeDisjunctionUnadorned && kind == "disjunction:unadorned-force" {
+		return s.optimizeDisjunctionUnadorned(octx, 0)
 	}
 
 	return octx, nil
@@ -286,6 +290,7 @@ OUTER:
 		}
 	}
 
+	atomic.AddUint64(&o.snapshot.parent.stats.TotTermSearchersStarted, uint64(1))
 	return oTFR, nil
 }
 
@@ -296,9 +301,12 @@ OUTER:
 // term-vectors are not required, and instead only the internal-id's
 // are needed.
 func (s *IndexSnapshotTermFieldReader) optimizeDisjunctionUnadorned(
-	octx index.OptimizableContext) (index.OptimizableContext, error) {
+	octx index.OptimizableContext, minChildCardinality uint64) (index.OptimizableContext, error) {
 	if octx == nil {
-		octx = &OptimizeTFRDisjunctionUnadorned{snapshot: s.snapshot}
+		octx = &OptimizeTFRDisjunctionUnadorned{
+			snapshot: s.snapshot,
+			minChildCardinality: minChildCardinality,
+		}
 	}
 
 	o, ok := octx.(*OptimizeTFRDisjunctionUnadorned)
@@ -319,6 +327,8 @@ type OptimizeTFRDisjunctionUnadorned struct {
 	snapshot *IndexSnapshot
 
 	tfrs []*IndexSnapshotTermFieldReader
+
+	minChildCardinality uint64
 }
 
 var OptimizeTFRDisjunctionUnadornedTerm = []byte("<disjunction:unadorned>")
@@ -353,7 +363,7 @@ func (o *OptimizeTFRDisjunctionUnadorned) Finish() (rv index.Optimized, err erro
 		// Heuristic to skip the optimization if all the constituent
 		// bitmaps are too small, where the processing & resource
 		// overhead to create the OR'ed bitmap outweighs the benefit.
-		if cMax < OptimizeDisjunctionUnadornedMinChildCardinality {
+		if cMax < o.minChildCardinality {
 			return nil, nil
 		}
 	}
@@ -416,5 +426,6 @@ func (o *OptimizeTFRDisjunctionUnadorned) Finish() (rv index.Optimized, err erro
 		}
 	}
 
+	atomic.AddUint64(&o.snapshot.parent.stats.TotTermSearchersStarted, uint64(1))
 	return oTFR, nil
 }
